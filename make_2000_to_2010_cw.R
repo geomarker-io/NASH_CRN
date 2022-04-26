70bc6570fa56c953a061dc20bde66bd442a5af2b
Cole Brokamp <cole.brokamp@gmail.com>
2022-04-26 09:42:50 -0400

    first commit

---
+++ make_2000_to_2010_cw.R
@@ -0,0 +1,122 @@
+library(tidyverse)
+library(sf)
+
+d_states_abb <- tigris::states() %>%
+  sf::st_drop_geometry() %>%
+  filter(! NAME %in% c('American Samoa', 'Guam', 'Northern Mariana Islands',
+                             'Puerto Rico', 'U.S. Minor Outlying Islands',
+                             'U.S. Virgin Islands', 'United States Virgin Islands',
+                       'Commonwealth of the Northern Mariana Islands')) %>%
+  select(abb = STUSPS,
+         fips = GEOID) %>%
+  mutate(abb = tolower(abb))
+
+states_abb <- glue::glue('{d_states_abb$abb}{d_states_abb$fips}')
+dwnld_url <- glue::glue('https://www2.census.gov/geo/docs/maps-data/data/rel/trf_txt/{states_abb}trf.txt')
+dest <- glue::glue("2000_2010_cw/{d_states_abb$abb}.txt")
+
+purrr::walk2(dwnld_url, dest, ~download.file(.x, destfile = .y))
+
+cw_col_names = c("STATE00",
+              "COUNTY00",
+              "TRACT00",
+              "GEOID00",
+              "POP00",
+              "HU00",
+              "PART00",
+              "AREA00",
+              "AREALAND00",
+              "STATE10",
+              "COUNTY10",
+              "TRACT10",
+              "GEOID10",
+              "POP10",
+              "HU10",
+              "PART10",
+              "AREA10",
+              "AREALAND10",
+              'AREAPT',
+              "AREALANDPT",
+              "AREAPCT00PT",
+              "AREALANDPCT00PT",
+              "AREAPCT10PT",
+              "AREALANDPCT10PT",
+              "POP10PT",
+              "POPPCT00",
+              "POPPCT10",
+              "HU10PT",
+              "HUPCT00",
+              "HUPCT10")
+
+cw <- purrr::map(dest, ~read_delim(.x, delim = ",", col_names = cw_col_names,
+                                   col_types = list(col_character(),
+                                                    col_character(),
+                                                    col_character(),
+                                                    col_character(),
+                                                    col_double(),
+                                                    col_double(),
+                                                    col_character(),
+                                                    col_double(),
+                                                    col_character(),
+                                                    col_character(),
+                                                    col_character(),
+                                                    col_character(),
+                                                    col_character(),
+                                                    col_double(),
+                                                    col_double(),
+                                                    col_character(),
+                                                    col_double(), col_double(), col_double(),
+                                                    col_double(), col_double(), col_double(),
+                                                    col_double(), col_double(), col_double(),
+                                                    col_double(), col_double(), col_double(),
+                                                    col_double(), col_double()
+                                                    )))
+
+cw <- bind_rows(cw)
+cw
+
+# hc00 <- tigris::tracts(state = 'ohio', county = 'hamilton', year = 2000)
+# hc10 <- tigris::tracts(state = 'ohio', county = 'hamilton', year = 2010)
+#
+# mapview::mapviewOptions(fgb = FALSE)
+# mapview::mapview(hc00) + mapview::mapview(hc10)
+#
+# cw %>%
+#   filter(GEOID10 == "39061026400") %>%
+#   select(GEOID00, GEOID10, AREALANDPCT10PT)
+#
+# cw %>%
+#   filter(GEOID00 == "39061004601") %>%
+#   select(GEOID00, GEOID10, AREALANDPCT10PT)
+
+cw <- cw %>%
+  select(census_tract_id_2000 = GEOID00,
+         census_tract_id_2010 = GEOID10,
+         weight_inverse = AREALANDPCT10PT) %>%
+  mutate(weight_inverse = weight_inverse / 100) %>%
+  filter(weight_inverse > 0.05)
+
+# adjustment so that weights from one tract add up to one
+cw_sum <- cw %>% 
+  group_by(census_tract_id_2010) %>% 
+  summarize(weight_inverse_sum = sum(weight_inverse))
+
+cw <- left_join(cw, cw_sum, by='census_tract_id_2010') %>% 
+  mutate(weight_inverse = weight_inverse / weight_inverse_sum) %>% 
+  select(-weight_inverse_sum)
+
+saveRDS(cw, 'Data/2000_to_2010_tract_cw.rds')
+
+# example
+# fake_data <- tibble(census_tract_id_2000 = c("39061004601",  # 1 2000 tract to 2 2020 tracts
+#                                             "39061000301", "39061000302", "39061000800"), # 3 2000 tracts to 1 2010 tract
+#                    population_2000 = c(100, 30, 40, 50))
+
+#fake_data %>%
+#  left_join(cw, by = "census_tract_id_2000") %>%
+#  mutate(new_pop = population_2000 * weight_inverse) %>%
+#  group_by(census_tract_id_2010) %>%
+#  summarize(new_pop = sum(new_pop))
+
+
+
---
+++ mua_csd_overlap_tract.R
@@ -0,0 +1,91 @@
+library(tidyverse)
+library(sf)
+
+url <- "https://data.hrsa.gov/DataDownload/DD_Files/MUA_DET.csv"
+destfile <- "MUA.csv"
+
+download.file(url, destfile=destfile)
+
+d <- read_csv(destfile)
+
+csd_mua <- d %>%
+  filter(`MUA/P Status Code` %in% c("P", "D"), # keep only designated and proposed for withdrawal
+         `Designation Type` %in% c("Medically Underserved Area",
+                                   "Medically Underserved Area – Governor’s Exception"),
+         `Medically Underserved Area/Population (MUA/P) Component Geographic Type Description` == "County Subdivision") %>%
+  mutate(mua = 'Yes') %>%
+  select(csd = `County Subdivision FIPS Code`,
+         mua)
+
+# csd_mua_oh <- csd_mua %>%
+#   filter(substr(csd, 1, 2) == '39')
+#
+# csd <- tigris::county_subdivisions(state = 'oh') %>%
+#   select(csd = GEOID)
+#
+# tracts <- tigris::tracts(state = 'oh') %>%
+#   select(tract_id = GEOID)
+#
+# csd_mua_oh <- left_join(csd_mua_oh, csd) %>%
+#   st_as_sf()
+#
+# mua_oh_tracts_overlaps <- st_join(tracts, csd_mua_oh, join = st_overlaps)
+# mua_oh_tracts_contains <- st_join(tracts, csd_mua_oh, join = st_contains)
+# mua_oh_tracts_within <- st_join(tracts, csd_mua_oh, join = st_within)
+#
+# mua_oh_tracts <- mua_oh_tracts_overlaps %>%
+#   left_join(mua_oh_tracts_contains %>% st_drop_geometry(), by = 'tract_id') %>%
+#   left_join(mua_oh_tracts_within %>% st_drop_geometry(), by = 'tract_id')
+#
+# mua_oh_tracts <- mua_oh_tracts %>%
+#   mutate(overlaps_csd_mua = ifelse(mua.x == "Yes" | mua.y == "Yes" | mua == "Yes", "Yes", "No"),
+#          overlaps_csd_mua = ifelse(is.na(overlaps_csd_mua), "No", overlaps_csd_mua)) %>%
+#   select(tract_id, overlaps_csd_mua)
+#
+# mapview::mapview(mua_oh_tracts, zcol = 'overlaps_csd_mua') +
+#   mapview::mapview(csd_mua_oh)
+#
+# mua_oh_tracts %>%
+#   st_drop_geometry()
+states <- tigris::states() %>%
+  st_drop_geometry() %>%
+  select(NAME) %>%
+  filter(!NAME %in% c('United States Virgin Islands',
+                      'Commonwealth of the Northern Mariana Islands',
+                      'Guam', 'American Samoa', 'Puerto Rico'))
+
+csd <- map(states$NAME, ~tigris::county_subdivisions(state = .x, year=2010)) %>%
+  bind_rows() %>%
+  select(csd = GEOID)
+
+tracts <- map(states$NAME, ~tigris::tracts(state = .x, year=2010)) %>%
+  bind_rows() %>%
+  select(tract_id = GEOID)
+
+csd_mua <- left_join(csd_mua, csd) %>%
+  st_as_sf()
+
+mua_tracts_overlaps <- st_join(tracts, csd_mua, join = st_overlaps)
+mua_tracts_contains <- st_join(tracts, csd_mua, join = st_contains)
+mua_tracts_within <- st_join(tracts, csd_mua, join = st_within)
+
+mua_tracts <- mua_tracts_overlaps %>%
+  left_join(mua_tracts_contains %>% st_drop_geometry(), by = 'tract_id') %>%
+  left_join(mua_tracts_within %>% st_drop_geometry(), by = 'tract_id')
+
+mua_tracts <- mua_tracts %>%
+  mutate(overlaps_csd_mua = ifelse(mua.x == "Yes" | mua.y == "Yes" | mua == "Yes", "Yes", "No"),
+         overlaps_csd_mua = ifelse(is.na(overlaps_csd_mua), "No", overlaps_csd_mua)) %>%
+  select(tract_id, overlaps_csd_mua)
+
+t <- mua_tracts %>%
+  filter(substr(tract_id, 1, 2) == '18')
+
+mapview::mapview(t, zcol = 'overlaps_csd_mua') +
+  mapview::mapview(csd_mua)
+
+mua_tracts <- mua_tracts %>%
+  st_drop_geometry()
+
+saveRDS(mua_tracts, 'mua_csd_overlap_tract.rds')
+
