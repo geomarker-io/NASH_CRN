90cdb78c36b349f2353a146c71fbd9a446011868
Cole Brokamp <cole.brokamp@gmail.com>
2022-05-10 12:21:14 -0400

    include file names of scripts as dependencies; fix food data

--- Makefile
+++ Makefile
@@ -1,33 +1,28 @@
-all: rds csv
-
-csv: rds
-	R -e "write.csv(readRDS('data/nash_crn_census_data_2010.rds'), 'data/nash_crn_census_data_2010.csv', row.names = F)"
-	R -e "write.csv(readRDS('data/nash_crn_census_data_2020.rds'), 'data/nash_crn_census_data_2020.csv', row.names = F)"
-
-rds: data/nash_crn_census_data_2010.rds data/nash_crn_census_data_2020.rds csv
+all: data/nash_crn_census_data_2010.rds data/nash_crn_census_data_2020.rds csv
+	echo "you did it!"
 
 clean:
 	rm -rfv raw-data
 
-data/states.rds:
+data/states.rds: R/make_tracts.R
 	Rscript R/make_tracts.R
 
-data/tracts.rds: 
+data/tracts.rds: R/make_tracts.R
 	Rscript R/make_tracts.R
 
-data/acs.rds: data/states.rds
+data/acs.rds: data/states.rds R/make_acs_data.R
 	Rscript R/make_acs_data.R
 
-data/ejscreen.rds:
+data/ejscreen.rds: R/make_ejscreen_data.R
 	Rscript R/make_ejscreen_data.R
 
-data/2000_to_2010_tract_cw.rds:
+data/2000_to_2010_tract_cw.rds: R/make_2000_to_2010_cw.R
 	Rscript R/make_2000_to_2010_cw.R
 
-data/food.rds: data/2000_to_2010_tract_cw.rds
+data/food.rds: data/2000_to_2010_tract_cw.rds R/make_food_data.R
 	Rscript R/make_food_data.R
 
-data/index.rds: 
+data/index.rds: R/make_index_data.R
 	Rscript R/make_index_data.R
 
 # data/mua.rds: 
@@ -36,9 +31,12 @@ data/index.rds:
 data/nash_crn_census_data_2010.rds: data/tracts.rds data/acs.rds data/ejscreen.rds data/food.rds data/index.rds
 	Rscript R/join_all_saved_data.R
 
-data/2010_to_2020_tract_cw.rds:
+data/2010_to_2020_tract_cw.rds: R/make_2010_to_2020_cw.R
 	Rscript R/make_2010_to_2020_cw.R
 
-data/nash_crn_census_data_2020.rds: data/2010_to_2020_tract_cw.rds
+data/nash_crn_census_data_2020.rds: data/2010_to_2020_tract_cw.rds R/transform_data_to_2020_census_tract_geography.R
 	Rscript R/transform_data_to_2020_census_tract_geography.R
 
+csv: data/nash_crn_census_data_2010.rds data/nash_crn_census_data_2020.rds
+	R -e "write.csv(readRDS('data/nash_crn_census_data_2010.rds'), 'data/nash_crn_census_data_2010.csv', row.names = F)"
+	R -e "write.csv(readRDS('data/nash_crn_census_data_2020.rds'), 'data/nash_crn_census_data_2020.csv', row.names = F)"
--- R/make_food_data.R
+++ R/make_food_data.R
@@ -1,8 +1,12 @@
 library(dplyr)
 
-download.file(url = "https://stacks.cdc.gov/view/cdc/61367/cdc_61367_DS2.xls",
-              destfile = "raw-data/mRFEI.xls",
-              mode = "wb")
+if (!file.exists("raw-data/mRFEI.xls")) {
+  download.file(
+    url = "https://stacks.cdc.gov/view/cdc/61367/cdc_61367_DS2.xls",
+    destfile = "raw-data/mRFEI.xls",
+    mode = "wb"
+  )
+}
 
 cw <- readRDS("data/2000_to_2010_tract_cw.rds")
 
@@ -18,11 +22,13 @@ mrfei <-
   summarize(mrfei = sum(mrfei2010)) %>%
   rename(census_tract_fips = census_tract_id_2010)
 
-download.file(
-  url = "https://www.ers.usda.gov/webdocs/DataFiles/80591/FoodAccessResearchAtlasData2019.xlsx?v=5255",
-  destfile = "raw-data/food_atlas.xlsx",
-  mode = "wb"
-)
+if (!file.exists("raw-data/food_atlas.xlsx")) {
+  download.file(
+    url = "https://www.ers.usda.gov/webdocs/DataFiles/80591/FoodAccessResearchAtlasData2019.xlsx?v=5255",
+    destfile = "raw-data/food_atlas.xlsx",
+    mode = "wb"
+  )
+}
 
 food_atlas <-
   readxl::read_excel("raw-data/food_atlas.xlsx", sheet = 3, na = c("NULL")) %>%
@@ -39,17 +45,23 @@ food_atlas <-
   select(census_tract_fips, usda_low_food_access_flag, usda_low_food_access_pct)
 
 # feedingamerica.org Data have to be requested; not directly downloadable
-# TODO add this in manually and put download process/url here in comments
 food_ins <-
-  readxl::read_excel("data/source/FANO Projections - March 2021 - Food Insecurity - v2.xlsx", sheet = " County - 2020 Projections") %>%
+  readxl::read_excel("data/source/fano_projections_march_2021_food_insecurity_v2.xlsx",
+    sheet = " County - 2020 Projections"
+  ) %>%
   mutate(FIPS_char = as.character(FIPS)) %>%
-  mutate(state_county_fips = ifelse(str_length(FIPS_char) == 4,
-    str_c("0", FIPS_char, sep = ""),
+  mutate(state_county_fips = ifelse(stringr::str_length(FIPS_char) == 4,
+    stringr::str_c("0", FIPS_char, sep = ""),
     FIPS_char
   )) %>%
   select(state_county_fips, food_insecurity_pct = `2019 Food Insecurity %`)
 
+food_data <- full_join(mrfei, food_atlas, by = "census_tract_fips")
+
 food_data <-
-  purrr::reduce(list(mrfei, food_atlas, food_ins), full_join, by = "census_tract_fips")
+  food_data |>
+  mutate(state_county_fips = substr(census_tract_fips, 1, 5)) |>
+  full_join(food_ins, by = "state_county_fips") |>
+  select(-state_county_fips)
 
 saveRDS(food_data, "data/food.rds")
