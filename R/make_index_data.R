b473014cba6faa4f91527625d044182e150b4a3b
qing_duan <qing.duan@cchmc.org>
2022-05-13 12:22:35 -0400

    Added codes to check if packages are installed or not before calling library()

--- R/make_index_data.R
+++ R/make_index_data.R
@@ -1,3 +1,9 @@
+.cran_packages <- c("dplyr")
+.inst <- .cran_packages %in% installed.packages()
+if(any(!.inst)) {
+  install.packages(.cran_packages[!.inst], repos = "http://cran.us.r-project.org")
+}
+
 library(dplyr)
 options(timeout = 1000)
 
